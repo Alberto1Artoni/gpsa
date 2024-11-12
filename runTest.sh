5788d6bccefe7683e067443946926d3f68488bbe
Alberto Artoni <Alberto.Artoni@polimi.it>
2024-11-12 08:49:58 +0100

    updating exec

--- runTest.sh
+++ runTest.sh
@@ -6,7 +6,16 @@ do
     do 
         echo "[runall -- running with]: OMP_NUM_THREADS=$i"
         export OMP_NUM_THREADS=$i
-        ./bin/gpsa --x data/X2.txt --y data/Y2.txt --grain-size $grain_size --exec-mode 3 >> timings
+        ./bin/gpsa --x data/X2.txt --y data/Y2.txt --grain-size $grain_size --exec-mode 2 >> timingsLoop
+    done
+    for i in 1 2 4 8 16
+    do 
+        echo "[runall -- running with]: OMP_NUM_THREADS=$i"
+        export OMP_NUM_THREADS=$i
+        ./bin/gpsa --x data/X2.txt --y data/Y2.txt --grain-size $grain_size --exec-mode 3 >> timingsTask
     done
 done
 
+
+cat timingsTask | grep completed | cut -d " " -f 7 > timeTask
+cat timingsLoop | grep completed | cut -d " " -f 6 > timeLoop
